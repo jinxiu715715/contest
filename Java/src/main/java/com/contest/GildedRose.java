331c4e62d0858c129c1406c62907ef516f1c7c44
jinxiu715715 <xujinlong0715@163.com>
2019-03-11 20:06:09 +0800

    test The Quality of an item is never negative success

--- Java/src/main/java/com/contest/GildedRose.java
+++ Java/src/main/java/com/contest/GildedRose.java
@@ -1,22 +1,27 @@
 package com.contest;
 
 class GildedRose {
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-    	
-    	for (int i = 0; i < items.length; i++) {
-			if(items[i].sellIn<= 0) {
-				items[i].quality -=2;
-			}else {
-				 items[i].sellIn = 9;
-				 items[i].quality = 19;
+	public void updateQuality() {
+
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].sellIn <= 0) {
+				if (items[i].quality > 2) {
+					items[i].quality -= 2;
+				} else {
+					items[i].quality = 0;
+				}
+
+			} else {
+				items[i].sellIn = 9;
+				items[i].quality = 19;
 			}
 		}
-       
-    }
+
+	}
 }
