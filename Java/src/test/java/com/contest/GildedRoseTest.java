bcb0610be218fc5b59acd7fba3a32cfdd6deaf5e
jinxiu715715 <xujinlong0715@163.com>
2019-03-11 19:50:13 +0800

    test normalgoods date passed

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -4,16 +4,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GildedRoseTest {
 
-    @Test
-    public void should_fix_me() {
-        Item[] items = new Item[] { new Item("normal", 10, 20) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
+	@Test
+	public void should_fix_me() {
+		Item[] items = new Item[] { new Item("normal", 10, 20) };
+		GildedRose gildedRose = new GildedRose(items);
 
-        assertEquals(9, (gildedRose.items[0]).sellIn);
-        assertEquals(19, gildedRose.items[0].quality);
-    }
+		gildedRose.updateQuality();
+
+		assertEquals(9, (gildedRose.items[0]).sellIn);
+		assertEquals(19, gildedRose.items[0].quality);
+	}
+
+	// should_return_ticket_when_one_car_park_given_parkinglot_have_restspace()
+	// Quality degrades twice as fast
+	@Test
+	public void should_quality_reduce_two_when_one_shop_given_normal_goods_date_passed() {
+
+		NormalGoods[] items = new NormalGoods[] { new NormalGoods("normal1", 0, 20),
+				new NormalGoods("normal2", 0, 10) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		gildedRose.updateQuality();
+		assertEquals(18, (gildedRose.items[0]).sellIn);
+		assertEquals(8, gildedRose.items[0].quality);
+	}
 }
